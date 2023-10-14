76004b24290517c6c51d5883cb59cb25600755c6
omame <me@omame.xyz>
2023-10-14 18:43:21 +0200

    libk: memcpy, strcpy

--- Kernel/libk.c
+++ Kernel/libk.c
@@ -9,3 +9,17 @@ void *memset(void *dest, int ch, uint64_t count) {
     }
     return dest;
 }
+
+char *strcpy(char *dest, const char *source) {
+    char *p = dest;
+    while ((*dest++ = *source++));
+    return p;
+}
+
+void *memcpy(void *dest, const void *src, uint64_t n) {
+    uint8_t *ptr = dest;
+    const uint8_t *ptr2 = src;
+    while (n--)
+        *ptr++ = *ptr2++;
+    return dest;
+}
--- Kernel/libk.h
+++ Kernel/libk.h
@@ -8,5 +8,7 @@
 extern bootproto_handoff_t *g_handoff;
 
 void *memset(void *dest, int ch, uint64_t count);
+void *memcpy(void *dest, const void *src, uint64_t n);
+char *strcpy(char *dest, const char *source);
 
 #endif
